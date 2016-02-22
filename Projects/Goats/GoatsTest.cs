a22a0bf8f9990ea491f3e52116950caedf3c98da
simonalucineti <sim06na@yahoo.com>
2016-02-22 16:14:58 +0200

    Calculates Forage Supply

---
+++ Projects/Goats/GoatsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Goats
+{
+    [TestClass]
+    public class GoatsTest
+    {
+        [TestMethod]
+        public void KilosOfHayForActualNumberOfGoats()
+        {
+            int actualKilosOfHay = CalculateForageSupply(3, 5, 105, 7, 9);
+            Assert.AreEqual(441, actualKilosOfHay);
+        }
+        int CalculateForageSupply(int initialDays, int initialNumberOfGoats, int initialForage, int actualDays, int actualNumberOfGoats)
+        {
+            int kilosOfHayForInitialNumberOfGoats = (actualDays * initialForage) / initialDays;
+            return (actualNumberOfGoats * kilosOfHayForInitialNumberOfGoats) / initialNumberOfGoats;
+        }
+    }
+
+}
