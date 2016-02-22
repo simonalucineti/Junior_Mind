8e5f58204832dd6c91f18b7dc503ac194999672f
simonalucineti <sim06na@yahoo.com>
2016-02-22 15:39:20 +0200

    Calculate Number Of  Red Mushrooms

---
+++ Projects/Mushrooms/Mushrooms.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mushrooms
+{
+    [TestClass]
+    public class Mushrooms
+    {
+        [TestMethod]
+        public void RedMushrooms()
+        {
+            int redMushrooms = CalculateRedMushrooms(320, 7);
+            Assert.AreEqual(280, redMushrooms);
+        }
+        int CalculateRedMushrooms( int total, int multiplier)
+        {
+            int whiteMushrooms = total / (multiplier + 1);
+            return multiplier*whiteMushrooms;
+        }
+    }
+}
