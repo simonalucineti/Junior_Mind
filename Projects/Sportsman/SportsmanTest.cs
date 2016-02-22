01ac79f871bc3121f9a88f61b7cce7b1864076cb
simonalucineti <sim06na@yahoo.com>
2016-02-22 15:29:05 +0200

    Calculate Number Of Reps

---
+++ Projects/Sportsman/SportsmanTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sportsman
+{
+    [TestClass]
+    public class SportsmanTest
+    {
+        [TestMethod]
+        public void NumberOfReps()
+        {
+            int reps = CalculateNumberOfReps(16);
+            Assert.AreEqual(256,reps);
+        }
+        int CalculateNumberOfReps(int round)
+        {
+        return round*round; 
+        }
+    }
+}
