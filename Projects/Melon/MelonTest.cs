946144bb6f6ac1da7a71838fac7c1e4cf4927fdb
simonalucineti <sim06na@yahoo.com>
2016-02-25 00:47:32 +0200

    Has An Even Number Of Kilos

---
+++ Projects/Melon/MelonTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Melon
+{
+    [TestClass]
+    public class MelonTest
+    {
+        [TestMethod]
+        public void AnEvenNumberOfKilos()
+        {
+            Assert.AreEqual("YES", HasAnEvenNumberOfKilos(12));
+        }
+        string HasAnEvenNumberOfKilos(int numberOfKilos)
+        {
+            string output = "";
+            output = numberOfKilos % 2 == 0 ? "YES" : "NO";
+            return output;
+        }
+    }
+}
