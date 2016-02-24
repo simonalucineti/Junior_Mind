7220b3a958d481006cf3ea3310b52ebdc6a759e9
simonalucineti <sim06na@yahoo.com>
2016-02-24 21:56:16 +0200

    Calculate Flight Distance Between Two Trains

---
+++ Projects/Train/TrainTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Train
+{
+    [TestClass]
+    public class TrainTest
+    {
+        [TestMethod]
+        public void FlightDistanceBetweenTwoTrains()
+        {
+            float flightDistance = CalculateFlightDistance(50);
+                Assert.AreEqual(25, flightDistance);
+        }
+        float CalculateFlightDistance(float distance)
+        {
+            return distance / 2;
+        }
+    }
+}
