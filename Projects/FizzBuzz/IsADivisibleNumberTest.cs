89778561df42ea95fe0464af4ad21f3de74fa6da
simonalucineti <sim06na@yahoo.com>
2016-02-25 00:06:17 +0200

    Number Is Divisible By Three

---
+++ Projects/FizzBuzz/IsADivisibleNumberTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsDivisibleByThree()
+        {
+            Assert.AreEqual("Fizz", IsDivisible(9));
+        }
+        string IsDivisible(int number)
+        {
+            string output = "";
+            if (number%3==0)
+             output = "Fizz";
+            return output;
+        }
+    }
+}
