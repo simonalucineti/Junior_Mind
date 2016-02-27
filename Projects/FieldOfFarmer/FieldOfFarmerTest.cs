e1f7bd3c17e20eec8bd1e503ad76a0b89166e09a
simonalucineti <sim06na@yahoo.com>
2016-02-27 22:03:06 +0200

    Calculate Initial Length Of The Field

---
+++ Projects/FieldOfFarmer/FieldOfFarmerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FieldOfFarmer
+{
+    [TestClass]
+    public class FieldOfFarmerTest
+    {
+
+        [TestMethod]
+        public void IntialValuesOfTheField()
+        {
+            Array expected = new[] { 2, -5 }; /*tinand cont ca aceasta problema se refera 
+                                               la un teren, solutia negativa nu poate fi 
+                                               luata in calcul, dar am luat-o in 
+                                               considerare ca exercitiu;*/
+            CollectionAssert.AreEqual(expected, CalculateInitialLengthOfField(10, 3));
+        }
+        Array CalculateInitialLengthOfField(int actualArea, int widthOfTheBoughtField)
+
+          {
+           int delta = (widthOfTheBoughtField * widthOfTheBoughtField) - 4 *(-actualArea);
+        //    if (delta > 0)
+        //    {
+                int initialLengthOfField = (int)(-widthOfTheBoughtField + Math.Sqrt(delta)) / 2;
+                int secondSolution = (int)(-widthOfTheBoughtField - Math.Sqrt(delta)) / 2;
+                return new int[] { initialLengthOfField, secondSolution };
+         //   }
+         //   return new int[] {0, 0}; 
+        }
+    }
+}
+    
+
