3200405396084693693164cc1149df6c193b0eae
simonalucineti <sim06na@yahoo.com>
2016-02-27 14:11:34 +0200

    The Number Is Less Than Ten

---
+++ Projects/ArabicToRoman/ArabicToRomanTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArabicToRoman
+{
+    [TestClass]
+    public class ArabicToRomanTest
+    {
+        [TestMethod]
+        public void NumberIsLessThanTen()
+        {
+            Assert.AreEqual("V", ConvertArabicToRoman(5));
+        }
+        string ConvertArabicToRoman(int number)
+        {
+
+            string[] units = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            return units[number];
+        }
+    }
+}
