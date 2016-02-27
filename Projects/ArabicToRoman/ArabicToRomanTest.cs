4fd92d3e7131326fc56ca1745950b6490ecccfca
simonalucineti <sim06na@yahoo.com>
2016-02-27 14:27:35 +0200

    The Number Is Equal To A Thousand

--- Projects/ArabicToRoman/ArabicToRomanTest.cs
+++ Projects/ArabicToRoman/ArabicToRomanTest.cs
@@ -9,19 +9,25 @@ namespace ArabicToRoman
         [TestMethod]
         public void NumberIsLessThanTen()
         {
-            Assert.AreEqual("V", ConvertArabicToRoman(5));
+            Assert.AreEqual("V", ConvertArabicNumbersToRomanNumerals(5));
         }
         [TestMethod]
         public void NumberIsLessThanAThousand()
         {
-            Assert.AreEqual("XLIX", ConvertArabicToRoman(49));
+            Assert.AreEqual("XLIX", ConvertArabicNumbersToRomanNumerals(49));
         }
-        string ConvertArabicToRoman(int number)
+        [TestMethod]
+        public void NumberIsEqualToAThousand()
+        {
+            Assert.AreEqual("C", ConvertArabicNumbersToRomanNumerals(100));
+        }
+        string ConvertArabicNumbersToRomanNumerals(int number)
         {
 
             string[] units = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
             string[] tens = { "","X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
-            return tens[number/10]+units[number%10];
+            string[] thousand = { "", "C" };
+            return thousand[number/100]+tens[(number/10)%10]+units[number%10];
         }
     }
 }
