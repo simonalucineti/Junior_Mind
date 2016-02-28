0868472059a96d7927061976204a6a96c164507f
simonalucineti <sim06na@yahoo.com>
2016-02-28 17:26:01 +0200

    Amount of payment for a minimum period of delay

---
+++ Projects/RentPerMonth/RentPerMonthTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RentPerMonth
+{
+    [TestClass]
+    public class RentPerMonthTest
+    {
+        [TestMethod]
+        public void AmountOfPaymentForAMinimumPeriodOfDelay()
+        {
+            Assert.AreEqual(102, CalculateTotalAmountOfPayment(100,1));
+        }
+       decimal CalculateTotalAmountOfPayment(int rentPerMonth, int lateDays)
+        {
+
+            decimal penalties = rentPerMonth * 2/100;
+            return rentPerMonth + penalties;
+        }
+    }
+}
