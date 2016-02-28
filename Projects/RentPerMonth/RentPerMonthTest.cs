ba354838b1dfd76b566cb9f82625fb9ce3498504
simonalucineti <sim06na@yahoo.com>
2016-02-28 21:26:41 +0200

    Calculate Amount Of Payment For A long Period Of Delay Extract a new function IsALongPeriodOfDelay Define new variables

--- Projects/RentPerMonth/RentPerMonthTest.cs
+++ Projects/RentPerMonth/RentPerMonthTest.cs
@@ -7,7 +7,7 @@ namespace RentPerMonth
     public class RentPerMonthTest
     {
         [TestMethod]
-        public void AmountOfPaymentForAMinimumPeriodOfDelay()
+        public void AmountOfPaymentForAShortPeriodOfDelay()
         {
             Assert.AreEqual(102, CalculateTotalAmountOfPayment(100,1));
         }
@@ -16,18 +16,36 @@ namespace RentPerMonth
         {
             Assert.AreEqual(105, CalculateTotalAmountOfPayment(100, 11));
         }
+
+        [TestMethod]
+        public void AmountOfPaymentForALongPeriodOfDelay()
+        {
+            Assert.AreEqual(110, CalculateTotalAmountOfPayment(100, 31));
+        }
         decimal CalculateTotalAmountOfPayment(int rentPerMonth, int lateDays)
         {
             var minimumPenalties = rentPerMonth * 2 / 100;
             var mediumPenalties = rentPerMonth * 5 / 100;
             var maximumPenalties = rentPerMonth * 10 / 100;
-            decimal penalties = IsAMediumPeriodOfDelay(lateDays) ? mediumPenalties : minimumPenalties;
-            return rentPerMonth + penalties;
+             
+            decimal penalties = minimumPenalties;
+
+            if (IsAMediumPeriodOfDelay(lateDays))
+                penalties = mediumPenalties;
+            if (IsALongPeriodOfDelay(lateDays))
+                penalties = maximumPenalties;
+            var totalAmountOfPayment= rentPerMonth + penalties;
+            return totalAmountOfPayment;
         }
 
         private static bool IsAMediumPeriodOfDelay(int lateDays)
         {
             return lateDays > 10;
         }
+        private bool IsALongPeriodOfDelay(int lateDays)
+        {
+            return lateDays > 30;
+        }
     }
+
 }
