bb4025e1b38716c9859e14a8a717000c7818fc2a
simonalucineti <sim06na@yahoo.com>
2016-03-02 21:55:32 +0200

    Define new variables; Determine The Next Meeting

---
+++ Projects/FizzBuzz/Lunch/LunchTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lunch
+{
+    [TestClass]
+    public class LunchTest
+    {
+        [TestMethod]
+        public void LunchToTheRestaurant()
+        {
+            Assert.AreEqual(12, DetermineTheNextMeeting(6, 4));
+        }
+        int DetermineTheNextMeeting(int frequentedByFriend, int frequentedByMe) {
+            //c.m.m.m.c a doua numere
+            var fFriend = frequentedByFriend;
+            var fMe = frequentedByMe;
+            while (fMe != 0)
+            {
+               int restOfDivison = fFriend % fMe;
+                fFriend = fMe;
+                fMe = restOfDivison;
+            }
+            return (frequentedByFriend*frequentedByMe)/fFriend;
+        }
+    }
+}
