bc85db85fecd1b1573f8c52814d6938fe9d0275d
simonalucineti <sim06na@yahoo.com>
2016-03-02 21:06:16 +0200

    Determine The Number Of Squares On The Chessboard

---
+++ Projects/Chessboard/ChessboardTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chessboard
+{
+    [TestClass]
+    public class ChessboardTest
+    {
+        [TestMethod]
+        public void NumberOFSquaresOnTheChessboard()
+        {
+            Assert.AreEqual(5, DetermineTheSquaresOnTheChessboard(2));
+        }
+        int DetermineTheSquaresOnTheChessboard(int lengthChessboard) {
+            int numberOfSquares = 0;
+            for (int i = 1; i <= lengthChessboard; i++)
+                numberOfSquares += i * i;
+            return numberOfSquares;
+        }
+    }
+}
