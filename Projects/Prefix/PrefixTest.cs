828c108c44f96671e10451b4f0dd4bffae4bcb87
simonalucineti <sim06na@yahoo.com>
2016-03-03 22:45:29 +0200

    No Common Prefix Found Common Prefix Found Same Length

--- Projects/Prefix/PrefixTest.cs
+++ Projects/Prefix/PrefixTest.cs
@@ -14,17 +14,33 @@ namespace Prefix
 
             CollectionAssert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void NoCommonPrefixFound()
+        {
+            string[] expected = new[] { "" };
+            string[] actual = new[] { FindCommonPrefix("aaba", "xyz") };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CommonPrefixFoundSameLength()
+        {
+            string[] expected = new[] { "aa" };
+            string[] actual = new[] { FindCommonPrefix("aaba", "aaab") };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
         String FindCommonPrefix(string firstWord, string secondWord)
         {
             string commonPrefix = "";
-            int i=0, j=0;
-            while ((i < firstWord.Length) && (j < secondWord.Length))
+            int i = 0;
+            while ((i < firstWord.Length) && (i<secondWord.Length))
             {
-                if (firstWord[i] == secondWord[j])
+                if (firstWord[i] == secondWord[i])
                     commonPrefix += firstWord[i];
                 i++;
-                j++;
-            }     
+               
+                    }     
             return commonPrefix;
         }
     }
