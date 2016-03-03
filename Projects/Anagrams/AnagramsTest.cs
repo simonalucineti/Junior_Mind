a20329b5c2ade3b2b478106c009c7a963bd34305
simonalucineti <sim06na@yahoo.com>
2016-03-04 00:05:03 +0200

    Cannot Generate Anagrams

---
+++ Projects/Anagrams/AnagramsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagrams
+{
+    [TestClass]
+    public class AnagramsTest
+    {
+        [TestMethod]
+        public void CannotGenerateAnagrams()
+        {
+            Assert.AreEqual(1, GenerateAnagrams("aaaa"));
+        }
+        int GenerateAnagrams (string word)
+        {
+            int numberOfAnagrams;
+            return numberOfAnagrams=1;
+        }
+    }
+}
