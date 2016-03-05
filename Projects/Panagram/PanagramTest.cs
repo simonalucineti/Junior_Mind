6e6d24485878b74d65388eb0a47de4d5b113560d
simonalucineti <sim06na@yahoo.com>
2016-03-05 22:24:13 +0200

    Is not a panagram - mumber, less words Extract a new function Check Characters

--- Projects/Panagram/PanagramTest.cs
+++ Projects/Panagram/PanagramTest.cs
@@ -22,35 +22,68 @@ namespace Panagram
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void IsNotAPanagramLessWords()
+        {
+            string expected = "Is Not A Panagram";
+            string actual = DetermineIfIsAPanagram("FOX jumps over the lazy dog");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsNotAPanagramNumber()
+        {
+            string expected = "Is Not A Panagram";
+            string actual = DetermineIfIsAPanagram("The QUI12 brown FoX juMPs over the lAZy dog");
+
+            Assert.AreEqual(expected, actual);
+        }
         string DetermineIfIsAPanagram (string phrase)
         {
             string answer = "";
             int[] apparition = new int[26];
-            for (int i = 0; i < phrase.Length; i++)
-            {
-                if (phrase[i] >= 'a' && phrase[i] <= 'z')
-                    apparition[phrase[i] - 'a']++;
-                else  if (phrase[i] >= 'A' && phrase[i] <= 'Z')
-                    apparition[phrase[i] - 'A']++;
-            }
+            CheckCharacters(phrase, apparition);
             answer = CheckApparition(answer, apparition);
 
             return answer;
         }
 
+        private static void CheckCharacters(string phrase, int[] apparition)
+        {
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if (char.IsNumber(phrase[i])) break;
+                else
+                {
+                    if (phrase[i] >= 'a' && phrase[i] <= 'z')
+                        apparition[phrase[i] - 'a']++;
+                    else if (phrase[i] >= 'A' && phrase[i] <= 'Z')
+                        apparition[phrase[i] - 'A']++;
+
+                }
+
+            }
+        }
+
         private static string CheckApparition(string answer, int[] apparition)
         {
             bool findChar = true;
-            for (int i = 0; i <= 25; i++)
+            int i = 0;
+            while (i <=25)
             {
-                findChar = false;
+               findChar = false;
                 if (apparition[i] != 0)
                     findChar = true;
+                else break;
+                i++;
             }
             if (findChar)
                 answer += "Is A Panagram";
             else
                 answer += "Is Not A Panagram";
+
             return answer;
         }
     }
