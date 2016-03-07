223b59e66c3bb4eec1d6dcff9266858913c5de8f
simonalucineti <sim06na@yahoo.com>
2016-03-08 00:35:04 +0200

    Determine The Cube Of K Number That Ending In  A Specific Format

---
+++ Projects/CubeNumber/CubeNumberTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CubeNumber
+{
+    [TestClass]
+    public class CubeNumberTest
+    {
+        [TestMethod]
+        public void TheSecondNumber()
+        {
+            
+            Assert.AreEqual (442, DetermineTheCubeOfKNumberThatEndingInASpecificFormat(2));
+        }
+        [TestMethod]
+        public void TheFourthNumber()
+        {
+            Assert.AreEqual (942, DetermineTheCubeOfKNumberThatEndingInASpecificFormat(4));
+        }
+        double DetermineTheCubeOfKNumberThatEndingInASpecificFormat (int k)
+        {
+            int number = 192;
+            int check = 1;
+            double cube=0;
+           
+            while (check < k)
+            {
+                number++;
+                cube = Math.Pow(number,3);
+                if (cube % 1000 == 888)
+                    check++;
+            }
+            return number;
+        }
+    }
+}
