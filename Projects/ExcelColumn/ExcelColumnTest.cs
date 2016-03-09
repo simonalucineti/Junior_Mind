e56dcf2c9c6e6476b5d2c67a7ed710a98ca8b9d6
simonalucineti <sim06na@yahoo.com>
2016-03-09 18:19:24 +0200

    Determine Excel Columns Extract a new function GetCharacter

---
+++ Projects/ExcelColumn/ExcelColumnTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelColumn
+{
+    [TestClass]
+    public class ExcelColumnTest
+    {
+        [TestMethod]
+        public void FindFirstColumn()
+        {
+            Assert.AreEqual("A", DetermineExcelColumn(1));
+        }
+        [TestMethod]
+        public void Find29thColumn()
+        {
+            Assert.AreEqual("AC", DetermineExcelColumn(29));
+        }
+        [TestMethod]
+        public void Find53thColumn()
+        {
+            Assert.AreEqual("BA", DetermineExcelColumn(53));
+        }
+        string DetermineExcelColumn(int columnNumber)
+        {
+            string columnName = "";
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                columnName = GetCharacter(columnNumber) + columnName;
+                columnNumber /= 26;
+            }
+
+            return columnName; 
+        }
+
+        private static char GetCharacter(int number)
+        {
+            return (char)('A' + columnNumber % 26);
+        }
+    }
+}
