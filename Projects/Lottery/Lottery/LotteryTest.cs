68bdda60ec6783d75f7bf52ee21a44d4e9f74b58
simonalucineti <sim06na@yahoo.com>
2016-03-13 18:27:50 +0200

    Probability For Second And Third Category

--- Projects/Lottery/Lottery/LotteryTest.cs
+++ Projects/Lottery/Lottery/LotteryTest.cs
@@ -38,9 +38,28 @@ namespace Lottery
         {
             Assert.AreEqual(1.519738361843625e-6, CalculateProbabilityToWin(5, 5, 40));
         }
+
+        [TestMethod]
+        public void SecondCategory()
+        {
+            Assert.AreEqual(1.844989951240777e-5, CalculateProbabilityToWin(5, 6, 49), 0.00001);
+        }
+
+        [TestMethod]
+        public void ThirdCategory()
+        {
+            Assert.AreEqual(9.68054211035818e-4, CalculateProbabilityToWin(4, 6, 49), 0.00001);
+        }
+
         double CalculateProbabilityToWin(double ticketNumbers, double winNumbers, double totalNumbers)
         {
-            return 1 / CombinationPossible(totalNumbers, winNumbers); 
+            double NotMatchesNumbers = totalNumbers - winNumbers;
+            double NotOnTheTicketNumbers = winNumbers - ticketNumbers;
+            double probability;
+            double firstNumerator = CombinationWinAndTicketNumbers(winNumbers, ticketNumbers);
+            double secondNumerator = CombinationNotOnTicketToNotMatchesNumbers(NotMatchesNumbers, NotOnTheTicketNumbers);
+            double denominator = CombinationPossible(totalNumbers, winNumbers);
+            return probability = (firstNumerator * secondNumerator) / denominator;
         }
         double Factorial(double number)
         {
