bc3227dfcff8f24b4d5569afa979ee3024521abd
simonalucineti <sim06na@yahoo.com>
2016-03-13 17:32:50 +0200

    Create and test function Factorial

---
+++ Projects/Lottery/Lottery/LotteryTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lottery
+{
+    [TestClass]
+    public class LotteryTest
+    {
+        [TestMethod]
+        public void TestFactorial()
+        {
+            Assert.AreEqual(24, Factorial(4));
+        }
+        double CalculateProbabilityToWin(double ticketNumbers, double winNumbers, double totalNumbers)
+        {
+            return 0;
+        }
+        double Factorial(double number)
+        {
+            double factorial = 1;
+            for (int i = 1; i <= number; i++)
+                factorial *= i;
+
+            return factorial;
+        }
+    }
+}
