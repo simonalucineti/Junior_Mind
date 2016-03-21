6024ff67f6a49107d879a25ce81796daf28da740
simonalucineti <sim06na@yahoo.com>
2016-03-21 15:47:34 +0200

    Create And Test Function Reverse Array

--- Projects/Conversion/Properties/AssemblyInfo.cs
+++ Projects/Conversion/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BaseConversion")]
+[assembly: AssemblyTitle("Conversion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BaseConversion")]
+[assembly: AssemblyProduct("Conversion")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5e98500b-e6ca-418b-a384-b6acfbcf40ad")]
+[assembly: Guid("7127fcdd-675b-40d5-9561-9f22c5c157bb")]
 
 // Version information for an assembly consists of the following four values:
 //
