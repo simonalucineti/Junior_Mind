3591f2f943bedc4023cdff26e43469b118e716ed
simonalucineti <sim06na@yahoo.com>
2016-03-21 16:13:07 +0200

    Create and test function Convert From Base Ten In Another Base

--- Projects/Conversion/ConversionTest.cs
+++ Projects/Conversion/ConversionTest.cs
@@ -11,6 +11,11 @@ namespace Base_Conversion
         {
             CollectionAssert.AreEqual(new byte[] { 1, 0, 0 }, Reverse(new byte[] { 0, 0, 1 }));
         }
+        [TestMethod]
+        public void ConvertingADecimalNumberInAnotherBaseTest()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0, 0 }, ConvertFromBaseTenInAnother(4,2));
+        }
         byte[] Reverse(byte[] array)
         {
             byte[] reverseArray = new byte[array.Length];
@@ -18,6 +23,17 @@ namespace Base_Conversion
                 reverseArray[i] = array[array.Length - i - 1];
             return reverseArray;
         }
+        byte[] ConvertFromBaseTenInAnother(int number, int baza)
+        {
+            byte[] numberConverted = { };
+            while (number != 0)
+            {
+                Array.Resize(ref numberConverted, numberConverted.Length + 1);
+                numberConverted[numberConverted.Length - 1] = (byte)(number % baza);
+                number /= baza;
+            }
+            return Reverse(numberConverted);
 
+        }
     }
 }
