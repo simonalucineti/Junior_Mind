b3ca349c95a8c417f11df76ee3381d4f804d670a
simonalucineti <sim06na@yahoo.com>
2016-03-24 00:13:38 +0200

    Create and test function Eliminate Zero From Beginning and function Add Zero For The Shorter

--- Projects/Conversion/ConversionTest.cs
+++ Projects/Conversion/ConversionTest.cs
@@ -21,7 +21,23 @@ namespace Base_Conversion
         {
             CollectionAssert.AreEqual(new byte[] { 0, 1, 0 }, OperationNot(new byte[] { 1, 0, 1 }));
         }
+        [TestMethod]
+        public void ReturnArrayWithZeroAtBeginningTest1()
+        {
+            Assert.AreEqual((byte) 0, AddZeroForTheShorter(new byte[] { 1, 1 }, 5));
+        }
 
+        [TestMethod]
+        public void ReturnArrayWithZeroAtBeginningTest2()
+        {
+            Assert.AreEqual((byte)1, AddZeroForTheShorter(new byte[] { 1,1,1,0 }, 3));
+        }
+
+        [TestMethod]
+        public void EliminateZeroFromBeginningTest()
+        {
+            CollectionAssert.AreEqual(new byte[] {1,0,1}, EliminateZeroFromBeginning(new byte[] { 0,0,1,0,1}));
+        }
 
         byte[] Reverse(byte[] array)
         {
@@ -48,6 +64,29 @@ namespace Base_Conversion
            for (int i = 0; i < array.Length; i++)
                 array[i] = (array[i] == 0) ? (byte)1 : (byte)0;
             return array;
+        }
+
+        byte AddZeroForTheShorter(byte[] array, int i)
+        {
+            if (i<array.Length) 
+                return array[array.Length - i - 1];
+            return (byte)0;
+        }
+
+        byte[] EliminateZeroFromBeginning(byte[] array)
+        {
+            int i;
+           
+            for (i=0; i<array.Length; i++)
+            {
+                if (array[i]!=0) break;
+            }
+            byte[] withoutZero = new byte[array.Length - i];
+            for (int k=i; k<array.Length; k++)
+            {
+                withoutZero[k - i] = array[k];
+            }
+            return withoutZero;
         }
     }
 }
