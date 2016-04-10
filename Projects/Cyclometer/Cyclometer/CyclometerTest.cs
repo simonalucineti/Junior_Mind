27db5f992ba03934dab5dcea9cb94d6752172882
simonalucineti <sim06na@yahoo.com>
2016-04-10 23:34:46 +0300

    Determine The Second and Name Of The Cyclist With  Max Speed

--- Projects/Cyclometer/Cyclometer/CyclometerTest.cs
+++ Projects/Cyclometer/Cyclometer/CyclometerTest.cs
@@ -12,7 +12,7 @@ namespace Cyclometer
             public int[] rotationsPerSecond;
             public int wheelDiameter;
 
-         public Cyclist (string name, int[] rotationsPerSecond, int wheelDiameter)
+            public Cyclist(string name, int[] rotationsPerSecond, int wheelDiameter)
             {
                 this.name = name;
                 this.rotationsPerSecond = rotationsPerSecond;
@@ -27,7 +27,7 @@ namespace Cyclometer
         [TestMethod]
         public void DetermineTotalDistanceForACyclistTest()
         {
-            var distance = new Cyclist("Andreea", new int[] {2, 2}, 50);
+            var distance = new Cyclist("Andreea", new int[] { 2, 2 }, 50);
             Assert.AreEqual(628, DetermineTotalDistanceForACyclist(distance));
         }
 
@@ -37,30 +37,76 @@ namespace Cyclometer
             var cyclists = new Cyclist[] { new Cyclist ("Andreea", new int[] { 2, 2}, 50),
                                            new Cyclist("Ciprian", new int[] { 1, 1, 2}, 50),
                                            new Cyclist("Maria", new int[] { 1, 2}, 50)};
-            Assert.AreEqual(1727, DetermineTotalDistanceForAll (cyclists));
+            Assert.AreEqual(1727, DetermineTotalDistanceForAll(cyclists));
         }
 
-     double CalculateDistancePerSecond (int rotationsPerSecond, int wheelDiameter)
+        [TestMethod]
+        public void FindSecondOfMaxSpeedTest()
+        {
+            var cyclist = new Cyclist("Andreea", new int[] { 1, 2, 5, 1, 4, 2, 3 }, 50);
+            Assert.AreEqual(5, FindSecondForMaxSpeed(cyclist));
+        }
+
+        [TestMethod]
+        public void FindSecondAndNameForMaxSpeedTest()
+        {
+            var cyclists = new Cyclist[] { new Cyclist ("Andreea", new int[] { 1,3,2,5,1}, 50),
+                                           new Cyclist("Ciprian", new int[] { 1, 2, 5}, 60),
+                                           new Cyclist("Maria", new int[] { 1,5,1,3,2}, 65)};
+            int second = 5;
+            Assert.AreEqual("Maria", FindSecondAndNameForMaxSpeed (cyclists, out second));
+        }
+
+        double CalculateDistancePerSecond(int rotationsPerSecond, int wheelDiameter)
         {
             return Math.Round(Math.PI * wheelDiameter * rotationsPerSecond);
         }
-     double DetermineTotalDistanceForACyclist(Cyclist cyclist)
+        double DetermineTotalDistanceForACyclist(Cyclist cyclist)
         {
             double totalDistance = 0;
-            for (int i=0; i< cyclist.rotationsPerSecond.Length; i++)
+            for (int i = 0; i < cyclist.rotationsPerSecond.Length; i++)
             {
-            totalDistance += CalculateDistancePerSecond(cyclist.rotationsPerSecond[i], cyclist.wheelDiameter);
+                totalDistance += CalculateDistancePerSecond(cyclist.rotationsPerSecond[i], cyclist.wheelDiameter);
             }
             return totalDistance;
-        } 
+        }
         double DetermineTotalDistanceForAll(Cyclist[] cyclist)
         {
             double totalDistance = 0;
-            for (int i=0; i<cyclist.Length; i++)
+            for (int i = 0; i < cyclist.Length; i++)
             {
                 totalDistance += DetermineTotalDistanceForACyclist(cyclist[i]);
             }
             return totalDistance;
+        }
+
+        int FindSecondForMaxSpeed(Cyclist cyclist)
+        {
+            int second = 0;
+            for (int i = 0; i < cyclist.rotationsPerSecond.Length; i++)
+            {
+                if (cyclist.rotationsPerSecond[i] > second)
+                    second = cyclist.rotationsPerSecond[i];
+            }
+            return second;
+        }
+        string FindSecondAndNameForMaxSpeed(Cyclist[] cyclist, out int maxSecond)
+        {
+            maxSecond = 0;
+            double maxSpeed = 0;
+            string name = "";
+            for (int i=0; i<cyclist.Length; i++)
+            {
+               int second = FindSecondForMaxSpeed(cyclist[i]);
+               double speed = CalculateDistancePerSecond(second, cyclist[i].wheelDiameter);
+                if (speed > maxSpeed)
+                {
+                    maxSpeed = speed;
+                    maxSecond = second;
+                    name = cyclist[i].name;
+                }
+            }
+            return name;
         }
     }
 }
