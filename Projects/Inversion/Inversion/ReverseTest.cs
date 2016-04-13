44656b916914d1ee7b34ea4909643cbe224f0a82
simonalucineti <sim06na@yahoo.com>
2016-04-13 23:14:58 +0300

    Create and test function Reverse String

---
+++ Projects/Inversion/Inversion/ReverseTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Inversion
+{
+    [TestClass]
+    public class ReverseTest
+    {
+        [TestMethod]
+        public void ReverseStringTest1()
+        {
+            Assert.AreEqual("a", ReverseString("a"));
+        }
+
+        [TestMethod]
+        public void ReverseStringTest2()
+        {
+            Assert.AreEqual("aeerdna", ReverseString("andreea"));
+        }
+
+        string ReverseString (string input)
+        {
+            var l = input[input.Length - 1];
+            return (input.Length == 1) ? input : l + ReverseString(input.Substring(0, input.Length - 1));
+        }
+    }
+}
