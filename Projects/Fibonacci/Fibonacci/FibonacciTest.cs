1725f636c83dd388dea1646050d7aad9f046a435
simonalucineti <sim06na@yahoo.com>
2016-04-13 22:54:22 +0300

    Create and test function Find Fibonacci

---
+++ Projects/Fibonacci/Fibonacci/FibonacciTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fibonacci
+{
+    [TestClass]
+    public class FibonacciTest
+    {
+        [TestMethod]
+        public void FindFibonacciTest1()
+        {
+            Assert.AreEqual(1, FindFibonacci(1));
+        }
+
+        [TestMethod]
+        public void FindFibonacciTest2()
+        {
+            Assert.AreEqual(5, FindFibonacci(5));
+        }
+
+        int FindFibonacci(int n)
+        {
+            return ((n == 0) || (n == 1)) ? n : FindFibonacci(n - 1) + FindFibonacci(n - 2);
+        }
+    }
+    
+}
